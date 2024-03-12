4112920083c5b3dc72d7d868a5ea604785aaee3e
Jako <jacobvoandersen@gmail.com>
2024-03-12 15:04:30 +0100

    Added playernames and made them show up over the player. Changed the menu to be center of screen

--- Scripts/Characters/playercontroller.gd
+++ Scripts/Characters/playercontroller.gd
@@ -7,20 +7,25 @@ extends CharacterBody2D
 @export var bulletTimer: float = 1
 @export var playerSprites = []
 @export var playerName: String = "Test"
-
+signal player_entered
 const bulletPre = preload ("res://Nodes/Characters/bullet.tscn")
 @onready var spr = $Sprite2D
 var canShoot: bool = true
 
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
+	if is_multiplayer_authority():
+		playerName = get_tree().get_first_node_in_group("test").text
+		#player_entered.emit(self)
 
 func _ready():
-	playerName = name
 	spr.texture = playerSprites[playerSpriteId-1][0]
 
 func _process(delta):
-	$Label.text = playerName
+	$Node2D/Label.text = playerName
+	$Node2D.global_rotation = 0
+	$Node2D.global_position.y = global_position.y - 48
+	$Node2D.global_position.x = global_position.x
 
 func _physics_process(delta):
 	velocity = Vector2.ZERO
@@ -30,13 +35,9 @@ func _physics_process(delta):
 	move_and_slide()
 
 func shoot():
-	var main = get_tree().current_scene
-	var bullet = bulletPre.instantiate()
-	var bulletSpr = bullet.get_child(0)
 	var cannon = get_node("Cannon")
-	main.add_child(bullet)
-	bulletSpr.texture = playerSprites[playerSpriteId-1][1]
-	bullet.transform = cannon.global_transform
+	spawnBullet.rpc(position, cannon.global_transform)
+	#bulletSpr.texture = playerSprites[playerSpriteId-1][1]
 
 func characterInput(delta):
 	if Input.is_action_pressed("p1_up"):
@@ -50,8 +51,16 @@ func characterInput(delta):
 
 	# var dir = Input.get_vector("p1_left", "p1_right", "p1_up", "p1_down")
 	# velocity = dir * speed
-	if Input.is_action_pressed("p"+str(playerId)+"_shoot") and canShoot:
+	if Input.is_action_pressed("p1_shoot") and canShoot:
 		shoot()
 		canShoot = false
 		await get_tree().create_timer(bulletTimer).timeout
 		canShoot = true
+
+@rpc("call_local")
+func spawnBullet(pos:Vector2, forward:Transform2D):
+	var main = get_tree().current_scene
+	var bullet = bulletPre.instantiate()
+	var bulletSpr = bullet.get_child(0)
+	main.add_child(bullet)
+	bullet.transform = forward
--- Scripts/Multiplayer/Multiplayer_test.gd
+++ Scripts/Multiplayer/Multiplayer_test.gd
@@ -6,21 +6,29 @@ var peer = ENetMultiplayerPeer.new()
 @export var player_scene: PackedScene
 @onready var menu = $"GuiMenu"
 @onready var ipArea = $"GuiMenu/VBoxContainer/IPAddress"
+@onready var nameBox = $"GuiMenu/VBoxContainer/NameBox"
 
 func _on_host_pressed():
-    peer.create_server(PORT)
-    multiplayer.multiplayer_peer = peer
-    multiplayer.peer_connected.connect(_add_player)
-    _add_player()
-    menu.visible = false
+	peer.create_server(PORT)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_add_player)
+	_add_player()
+	menu.visible = false
 
 func _add_player(id = 1):
-    var player = player_scene.instantiate()
-    player.name = str(id)
-    # call_deferred("add_child", player)
-    find_child("Player_Spawn").call_deferred("add_child", player)
-
+	var player = player_scene.instantiate()
+	player.name = str(id)
+	player.player_entered.connect(set_player_name)
+	# call_deferred("add_child", player)
+	find_child("Player_Spawn").call_deferred("add_child", player)
+func set_player_name(player):
+	player.playerName = nameBox.text
+	
 func _on_join_pressed():
-    peer.create_client(ipArea.text, PORT)
-    multiplayer.multiplayer_peer = peer
-    menu.visible = false
+	# var error = peer.create_client(ipArea.text, PORT)
+	var error = peer.create_client("localhost", PORT)
+	if error:
+		print("dis")
+	else:
+		multiplayer.multiplayer_peer = peer
+		menu.visible = false
