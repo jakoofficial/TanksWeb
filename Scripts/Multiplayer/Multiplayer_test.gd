c14f370feff03d05801aa55b9a014179948b9f63
Jako <jacobvoandersen@gmail.com>
2024-03-12 13:20:49 +0100

    Added ip area for connection

--- Scripts/Multiplayer/Multiplayer_test.gd
+++ Scripts/Multiplayer/Multiplayer_test.gd
@@ -4,12 +4,15 @@ const PORT = 8080
 var peer = ENetMultiplayerPeer.new()
 @export var playArea: PackedScene
 @export var player_scene: PackedScene
+@onready var menu = $"GuiMenu"
+@onready var ipArea = $"GuiMenu/VBoxContainer/IPAddress"
 
 func _on_host_pressed():
     peer.create_server(PORT)
     multiplayer.multiplayer_peer = peer
     multiplayer.peer_connected.connect(_add_player)
     _add_player()
+    menu.visible = false
 
 func _add_player(id = 1):
     var player = player_scene.instantiate()
@@ -18,5 +21,6 @@ func _add_player(id = 1):
     find_child("Player_Spawn").call_deferred("add_child", player)
 
 func _on_join_pressed():
-    peer.create_client("localhost", PORT)
+    peer.create_client(ipArea.text, PORT)
     multiplayer.multiplayer_peer = peer
+    menu.visible = false
